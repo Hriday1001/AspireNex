fe93e5f66d679882b6fcd464148be15c63a9eed8
Hriday1001 <hridaybhardwaj_se21a14_30@dtu.ac.in>
2024-07-13 00:09:41 +0530

    Cleanup

--- personalPortfolio/src/components/index.js
+++ personalPortfolio/src/components/index.js
@@ -1,7 +1,5 @@
 import About from "./About/About";
 import Contact from "./Contact/Contact";
-import Footer from "./Footer";
-import Header from "./Header";
 import Home from "./Home/Home";
 import Skills from "./Skills/Skills";
 import Projects from "./Projects/Projects";
@@ -10,8 +8,6 @@ import Projects from "./Projects/Projects";
 const components = {
     About,
     Contact,
-    Footer,
-    Header,
     Home,
     Skills,
     Projects
